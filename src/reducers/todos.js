70b76c4387c7ac3fd94e70c6c8278d2ec1791508
Bruno Parga <brunoparga@gmail.com>
2019-11-25 11:18:02 -0300

    Improve handling of updates

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -3,14 +3,11 @@ import { FETCH_TODOS, MARK_DONE, CREATE_TODO } from "../action_types";
 const initialState = []
 
 export default function todos(state = initialState, { type, payload }) {
-  switch(type){
+  switch(type) {
     case FETCH_TODOS:
       return payload;
     case MARK_DONE:
-      // this will be done the dumb way: have the API just return all the todos.
-      // TODO: do it the smart way - return just the new todo, update it in the
-      // state.
-      return payload;
+      return state.map(todo => todo.id === payload.id ? payload : todo);
     case CREATE_TODO:
       return [...state, payload]
     default:
